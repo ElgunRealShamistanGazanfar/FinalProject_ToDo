0ef93ee12d6098fd9cd4c177999388b8443e7ea5
Şəmistan Hüseynov <shamistan1999@gmail.com>
2020-07-20 16:19:53 +0400

    --liquibase added final

--- src/main/java/app/controller/MailController.java
+++ src/main/java/app/controller/MailController.java
@@ -1,18 +1,18 @@
-package app.controller;
-
-import app.entity.Mail;
-import app.service.Sender;
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-@AllArgsConstructor
-@RestController("/")
-public class MailController {
-    private Sender sender;
-
-    @PostMapping("/send")
-    public void sendMessage(@RequestParam("mail") String mail,@RequestParam("subject") String subject ,@RequestParam("text") String text) {
-        sender.sendMail(mail, subject, text);
-
-    }
-}
+//package app.controller;
+//
+//import app.entity.Mail;
+//import app.service.Sender;
+//import lombok.AllArgsConstructor;
+//import org.springframework.web.bind.annotation.*;
+//
+//@AllArgsConstructor
+//@RestController("/")
+//public class MailController {
+//    private Sender sender;
+//
+//    @PostMapping("/send")
+//    public void sendMessage(@RequestParam("mail") String mail,@RequestParam("subject") String subject ,@RequestParam("text") String text) {
+//        sender.sendMail(mail, subject, text);
+//
+//    }
+//}
